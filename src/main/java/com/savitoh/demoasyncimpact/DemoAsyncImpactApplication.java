e02ddd5c535861800558214fc38cd0895f57f319
Sávio Raires <savioraires10@gmail.com>
2021-03-06 22:44:21 -0300

    Removendo imports não utilizados

--- src/main/java/com/savitoh/demoasyncimpact/DemoAsyncImpactApplication.java
+++ src/main/java/com/savitoh/demoasyncimpact/DemoAsyncImpactApplication.java
@@ -1,10 +1,7 @@
 package com.savitoh.demoasyncimpact;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class DemoAsyncImpactApplication {
